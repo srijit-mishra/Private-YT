c7b711bc15cd9f6b4e2a301339eaf13c7f4c5332
Srijit <mishrasrijit@gmail.com>
2023-03-01 19:39:21 +0530

    YT Clone
    YouTube Clone made using React/MUI

---
+++ src/components/Loader.jsx
@@ -0,0 +1,17 @@
+import { Box, CircularProgress, Stack } from "@mui/material";
+import React from "react";
+
+const Loader = () => (
+  <Box minHeight="95vh">
+    <Stack
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      height="80vh"
+    >
+      <CircularProgress />
+    </Stack>
+  </Box>
+);
+
+export default Loader;
